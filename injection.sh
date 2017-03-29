cf70ab9475d4ac3681b35b11d21c71e42b27989d
Deen Aariff <daariff.records@gmail.com>
2017-03-28 19:48:25 -0700

    Update injection.sh

--- injection.sh
+++ injection.sh
@@ -1,10 +1,13 @@
+# Split $NIRMATA_SERVICE_PORTS into array
 space_delim=$(echo $NIRMATA_SERVICE_PORTS | sed "s/:/ /g")
-arr=($line)
+arr=($space_delim)
 
+# Search through Array for Name of Exposed Port ("HTTP");
 for((i=0;i<${#arr[@]};i++))
 do
     if (${arr[$i]} == "HTTP")
       then
+        # Assign Env Variable to Exposed Port on HOST
         HOST_PORT = ${arr[2]}
     fin
 done
